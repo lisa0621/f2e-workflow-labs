454e9199810c5f573c1feeec3e8c80cea66f3477
SHA-PAI LI <lisa0621@gmail.com>
2015-10-25 14:37:02 +0800

    jquery 動態載入css

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+var $ = require('jquery');
 var config = require('./gulp/config');
 
 console.log('Hello little little SHA, ' + config.assetsDir);
 
+// require('!style!css!./assets/styles.css');
+
+$(function(){
+	$('body').on('click', function(){
+		require('!style!css!./assets/styles.css');
+	});
+});
